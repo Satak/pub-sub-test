2f07d21ba2316e3e0033bd1abab1e4e55989f469
satak <sami.koskivaara@gmail.com>
2019-02-20 10:47:28 +0200

    Init

---
+++ src/main.py
@@ -0,0 +1,144 @@
+import datetime
+from google.cloud import pubsub_v1
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, abort
+import sqlite3
+from os import getenv
+
+app = Flask(__name__)
+app.secret_key = getenv('SECRET')
+
+project_id = getenv('GCP_PROJECT')
+subscription_name = getenv('SUBSCRIPTION_NAME')
+topic_name = getenv('TOPIC_NAME')
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_name)
+
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(project_id, subscription_name)
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def get_vms():
+    with sqlite3.connect('cmdb.sqlite') as con:
+        con.row_factory = dict_factory
+        cur = con.cursor()
+        cur.execute("SELECT vm_name, namespace, message FROM vms")
+        vms = cur.fetchall()
+        return vms
+
+
+def get_messages_sync():
+    response = subscriber.pull(subscription_path, max_messages=10, return_immediately=True)
+    return [
+        {
+            'message': received_message.message.data.decode(),
+            'namespace': received_message.message.attributes['namespace'],
+            'vm_name': received_message.message.attributes['vm_name'],
+            'action': received_message.message.attributes['action'],
+            'ack_id': received_message.ack_id
+        } for received_message in response.received_messages]
+
+
+def insert_cmdb_record(namespace, vm_name, message):
+    record = {
+        'namespace': namespace,
+        'vm_name': vm_name,
+        'message': message
+    }
+    try:
+        with sqlite3.connect('cmdb.sqlite') as con:
+            cur = con.cursor()
+            cur.execute(
+                """
+                  INSERT INTO vms (namespace, vm_name, message)
+                  VALUES (:namespace, :vm_name, :message)
+                """,
+                record
+            )
+            return {
+                'ok': True,
+                'message': 'record inserted successfully',
+                'status_code': 201
+            }
+    except Exception as err:
+        err_msg = f'Something went wrong while inserting data {err}'
+        return {
+            'ok': False,
+            'error': err_msg,
+            'status_code': 400
+        }
+
+
+def send_message(data, action, vm_name, namespace):
+    future = publisher.publish(
+        topic_path,
+        data=data.encode(),
+        action=action,
+        vm_name=vm_name,
+        namespace=namespace
+    )
+    return f'Data: "{data}" Action: "{action}" Name: "{vm_name}" Namespace: "{namespace}" ID: {future.result()}'
+
+
+@app.route('/')
+def root():
+    return redirect(url_for('message'))
+
+
+@app.route('/api/messages')
+def api_messages():
+    return jsonify(get_messages_sync())
+
+
+@app.route('/messages')
+def messages():
+    return render_template('messages.html', messages=get_messages_sync())
+
+
+@app.route('/cmdb')
+def cmdb_view():
+    vms = get_vms()
+    return render_template('cmdb.html', vms=vms)
+
+
+@app.route('/cmdb', methods=['POST'])
+def cmdb():
+    data = request.get_json(silent=True)
+    if data['action'] == 'create':
+        res = insert_cmdb_record(
+            namespace=data['namespace'],
+            vm_name=data['vm_name'],
+            message=data['message']
+        )
+        return jsonify(res), res['status_code']
+    else:
+        return jsonify({'error': 'action not supported yet'}), 400
+
+
+@app.route('/message')
+def message():
+    return render_template('message.html')
+
+
+@app.route('/message', methods=['POST'])
+def post_message():
+    message = request.form['message']
+    action = request.form['action']
+    vm_name = request.form['vm_name']
+    namespace = request.form['namespace']
+    n = datetime.datetime.now()
+    full_message = f'{n.hour}:{n.minute}:{n.second} - {message}'
+    res = send_message(data=full_message, action=action, vm_name=vm_name, namespace=namespace)
+    flash(res)
+    return redirect(url_for('message'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
---
+++ src/subscriber.py
@@ -0,0 +1,39 @@
+import time
+from os import getenv
+from google.cloud import pubsub_v1
+import requests
+
+project_id = getenv('GCP_PROJECT')
+subscription_name = getenv('SUBSCRIPTION_NAME')
+topic_name = getenv('TOPIC_NAME')
+
+url = 'http://localhost:5000/cmdb'  # change this to your dev server
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(project_id, subscription_name)
+
+
+def callback(message):
+    json_data = {
+        'namespace': message.attributes['namespace'],
+        'action': message.attributes['action'],
+        'message': message.data.decode(),
+        'vm_name': message.attributes['vm_name']
+    }
+    res = requests.post(url, json=json_data)
+    if res.ok:
+        print('Data:', message.data.decode())
+        print('Custom attributes:', message.attributes)
+        print('Single attribute (action):', message.attributes['action'])
+        print('Single attribute (namespace):', message.attributes['namespace'])
+        print('\n')
+        message.ack()
+    else:
+        print('Something went wrong:', str(res.content))
+
+subscriber.subscribe(subscription_path, callback=callback)
+
+# The subscriber is non-blocking. We must keep the main thread from
+# exiting to allow it to process messages asynchronously in the background.
+print(f'Listening for messages on {subscription_path}\n')
+while True:
+    time.sleep(60)
